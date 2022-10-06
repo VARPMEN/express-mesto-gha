85782b851901beebe05b6372c94032ec839114d6
Anton Prokhorov <varpmen@gmail.com>
2022-10-06 03:49:38 +0300

    fix/linkrege

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,7 +1,7 @@
 const { celebrate, Joi } = require('celebrate');
 
 // eslint-disable-next-line no-useless-escape
-const linkRegExp = /^(https?\:\/\/)(www)?[\w\.\_\~\:\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=]*\#?$/gi;
+const linkRegExp = /^(https?\:\/\/)(www)?[\w\.\_\~\:\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=]*\#?/;
 
 const idValidation = celebrate({
   params: Joi.object().keys({
