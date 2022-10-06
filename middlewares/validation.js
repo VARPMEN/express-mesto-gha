f2880e966125522a392028b65a78ce346a9917b0
Anton Prokhorov <varpmen@gmail.com>
2022-10-06 03:54:55 +0300

    fix/invalid url-card

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -12,7 +12,7 @@ const idValidation = celebrate({
 const cardValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required(),
+    link: Joi.string().required().pattern(linkRegExp),
   }),
 });
 
